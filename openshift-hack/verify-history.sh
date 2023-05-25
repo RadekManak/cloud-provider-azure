1a7e6186318bff335086ee2f9efa19ecdbd4bb28
Radek Maňák <rmanak@redhat.com>
2023-05-25 15:03:22 +0200

    UPSTREAM: <carry>: Prevent PRs not built on top of base branch from merging

--- openshift-hack/verify-history.sh
+++ openshift-hack/verify-history.sh
@@ -42,5 +42,19 @@ while read -r message; do
   echo "$message"
 done < <(git log "$check_base".."$check_sha" --pretty=%s --no-merges)
 
+if [[ "$CI" == "true" ]]; then
+    echo "Comparing ${PULL_BASE_REF}=$(git rev-parse --verify ${PULL_BASE_REF}^1) and ${PULL_BASE_SHA}"
+    echo git log for debugging
+    git log --all --decorate --oneline --graph
+    if [[ "$(git rev-parse --verify ${PULL_BASE_REF}^1)" != "$PULL_BASE_SHA" ]]; then
+        echo "Only pull requests that are based on the latest commit on the base branch are allowed to merge. To fix this, rebase your pull request."
+        echo
+        echo "Additional commits on ${PULL_BASE_REF} that are not included in this PR:"
+        git log ${PULL_BASE_REF} ^${PULL_PULL_SHA}
+        exit 1
+    fi
+    echo "This PR includes all commits from base branch."
+fi
+
 echo
 echo "All looks good"
